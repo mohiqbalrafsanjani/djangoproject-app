d99b4e8a2583598bd269e786bb05392a8a7922b5
Moh. Iqbal Rafsanjani <mohiqbalrafsanjani@gmail.com>
2022-11-09 18:29:03 +0700

    Fix error votes results

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -37,12 +37,12 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render (request, 'polls/detail.html', {
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     
